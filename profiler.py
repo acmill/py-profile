8b8f42623520f3ba41fd502915eb7ca15f6bff2b
Anthony Coleman Miller <mill2717@msu.edu>
2019-12-09 12:27:35 -0500

    calculate difference

--- profiler.py
+++ profiler.py
@@ -12,7 +12,7 @@ from time import perf_counter
 ####
 #   Spit out time values for two functions A, B tested on set of inputs size N
 ####
-def GetResults(A, B, N):
+def GetResults(A, B, N):        
 
     #   Generate stats in ms for function A
     meanA = statistics.mean(A)
@@ -23,6 +23,9 @@ def GetResults(A, B, N):
     meanB = statistics.mean(B)
     medianB = statistics.median(B)
     modeB = statistics.mode(B)
+
+    #   Comparison Metric
+
 
     #   Header
     print()
@@ -44,22 +47,42 @@ def GetResults(A, B, N):
     print(" -> Mode Time Value for B(): {:>30}".format(modeB),"ms" )
     print()
 
+    #   Compare A and B
+
+
 
     #   Compare mean values
-    if meanA < meanB: print("The mean time value for function A is lower: ", meanA,"ms")
-    elif meanA > meanB: print("The mean time value for function B is lower: ", meanB,"ms")
+    if meanA < meanB:
+        print("The mean time value for function A is lower: ", meanA,"ms")
+        print(" -> Difference: ", meanB - meanA,"ms")
+
+    elif meanA > meanB:
+        print("The mean time value for function B is lower: ", meanB,"ms")
+        print(" -> Difference: ", meanA - meanB,"ms")
     elif meanA == meanB: print("The mean time values were found to be equivalent.")
     print()
 
     #   Compare median values
-    if medianA < medianB: print("The median time value for function A is lower: ", medianA,"ms")
-    elif medianA > medianB: print("The median time value for function B is lower: ", medianB,"ms")
+    if medianA < medianB:
+        print("The median time value for function A is lower: ", medianA,"ms")
+        print(" -> Difference: ", medianB - medianA,"ms")
+
+    elif medianA > medianB:
+        print("The median time value for function B is lower: ", medianB,"ms")
+        print(" -> Difference: ", medianA - medianB,"ms")
+
     elif medianA == medianB: print("The median time values were found to be equivalent.")
     print()
 
     #   Compare mode values (if it is relevant to case)
-    if modeA < modeB: print("The mode time value for function A is lower: ", modeA,"ms")
-    elif modeA > modeB: print("The mode time value for function B is lower: ", modeB,"ms")
+    if modeA < modeB:
+        print("The mode time value for function A is lower: ", modeA,"ms")
+        print(" -> Difference: ", modeB - modeA,"ms")
+
+    elif modeA > modeB:
+        print("The mode time value for function B is lower: ", modeB,"ms")
+        print(" -> Difference: ", modeA - modeB,"ms")
+
     elif modeA == modeB: print("The mode time values were found to be equivalent.")
     print()
 
@@ -93,7 +116,7 @@ def main():
     initialClockProgram = perf_counter()
 
     #   This is a lot (change at will)
-    N = 10000000
+    N = 5000000
 
     #   Make a length N list of random numbers anywhere between 0 to 1 billion
     randomNumbers = [random.randint(0, 1000000000) for _ in range(N)]
